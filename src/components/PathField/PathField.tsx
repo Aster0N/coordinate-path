12d30f0adfab86ffdcf54141562b62d2f30d9072
Aster0N <nikvas131@gmail.com>
2025-02-07 13:57:47 +0300

    Fixed @ alias in vite config. Add PathField component

---
+++ src/components/PathField/PathField.tsx
@@ -0,0 +1,7 @@
+import cl from "./PathField.module.css";
+
+const PathField = () => {
+  return <div className={cl.field}></div>;
+};
+
+export default PathField;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,13 @@
+import PathField from "@/components/PathField/PathField.tsx";
 
 const Home = () => {
-	return (
-		<h1>Home page</h1>
-	)
-}
+  return (
+    <>
+      <h1>Home page</h1>
+      <hr />
+      <PathField />
+    </>
+  );
+};
 
-export default Home
+export default Home;
